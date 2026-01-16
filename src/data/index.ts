552b83bfceb945b806b6e49b1be2d6ae67da7300
Bartek Strach <iambartekstrach@gmail.com>
2026-01-16 19:07:11 +0100

    feat: components, types and data

---
+++ src/data/index.ts
@@ -0,0 +1,7 @@
+import { Product } from "@/types";
+
+export const MOCKED_PRODUCTS: Product[] = [
+  { id: "1", name: "Laptop", price: 999 },
+  { id: "2", name: "Mouse", price: 29 },
+  { id: "3", name: "Keyboard", price: 79 },
+];
---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export interface CartItem {
+  id: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+}
